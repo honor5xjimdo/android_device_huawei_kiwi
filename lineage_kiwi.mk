9ff827b82a4bf84bdf9307f13bb92d29d17acfc5
Flytreels <66539732+Flytreels@users.noreply.github.com>
2021-07-08 23:01:36 +0800

    Update lineage_kiwi.mk

--- lineage_kiwi.mk
+++ lineage_kiwi.mk
@@ -16,14 +16,14 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l.mk)
 
 # Inherit from kiwi device
 $(call inherit-product, device/huawei/kiwi/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+#$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := HONOR
